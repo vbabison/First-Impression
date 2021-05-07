57a045e1c892f7354fd6dd530073ca87e3870e7a
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-05-07 02:44:47 -0400

    Adding video, connection between two userIds, copy-to-clipboard

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
-const express = require('express')
+const express = require("express")
+const http = require("http")
 const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const { v4: uuidV4 } = require('uuid')
+const server = http.createServer(app)
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "http://localhost:3000",
+		methods: [ "GET", "POST" ]
+	}
+})
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
+
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
+})
+
+server.listen(5000, () => console.log("server is running on port 5000"))
