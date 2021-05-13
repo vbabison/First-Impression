5f447a53d1b6a28a184aec21fcb1208251f0268e
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-05-13 06:16:20 -0400

    database

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (name, email, password, is_online, connections, rating)
+VALUES ('Bobby Brown', 'bobby@example.com', 'password', true, 320, 4.8);
+INSERT INTO users (name, email, password)
+VALUES ('Jason Bourne', 'sergey@example.com', 'password', true, 224, 4.8);
+INSERT INTO users (name, email, password)
+VALUES ('Thomas Shelby', 'nick@example.com', 'password', false, 912, 4.9);
